f18a3a2dc5c8d78e37e470eea28b752d192dc568
DJ Enriquez <denrie.enriquezjr@gmail.com>
2018-01-24 23:40:14 -0800

    Add AWS region

--- src/sns.py
+++ src/sns.py
@@ -2,7 +2,7 @@ import boto3
 import os
 
 def publish(message):
-    client = boto3.client('sns')
+    client = boto3.client('sns', region_name=os.getenv('AWS_REGION', 'us-east-1'))
     response = client.publish(
         TopicArn=os.getenv('SNS_TOPIC_ARN', ''),
         Message=message,
