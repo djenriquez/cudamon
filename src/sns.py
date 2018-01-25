0a4bd5c991369be9a5cb3ebeb4b96db8e0a17016
DJ Enriquez <denrie.enriquezjr@gmail.com>
2018-01-25 00:47:21 -0800

    Use timebased timeouts

--- src/sns.py
+++ src/sns.py
@@ -1,28 +1,27 @@
 import boto3
 import os
+from datetime import datetime, timedelta
 
 class SNS:
     def __init__(self):
-        self.timeout = int(os.getenv('TIMEOUT', 10)) * 2
+        self.timeout = timedelta(minutes=int(os.getenv('TIMEOUT_MINS', 10)))
+        self.nextAlert = datetime.utcnow()
         self.alerted = False
     
     def publish(self, message):
-        client = boto3.client('sns', region_name=os.getenv('AWS_REGION', 'us-east-1'))
-        response = client.publish(
-            TopicArn=os.getenv('SNS_TOPIC_ARN', ''),
-            Message=message,
-            Subject='Miner Alert',
-            MessageStructure='raw'
-        )
-        self.alerted = True
+        if self.can_publish():
+            client = boto3.client('sns', region_name=os.getenv('AWS_REGION', 'us-east-1'))
+            response = client.publish(
+                TopicArn=os.getenv('SNS_TOPIC_ARN', ''),
+                Message=message,
+                Subject='Miner Alert',
+                MessageStructure='raw'
+            )
+            self.alerted = True
+            self.nextAlert = datetime.utcnow() + self.timeout
 
     def can_publish(self):
-        if self.alerted and self.timeout > 0:
-            self.timeout -= 1
+        if self.alerted and datetime.utcnow() < self.nextAlert:
             return False
         else:
-            self.reset_timeout()
             return True
-
-    def reset_timeout(self):
-        self.timeout = int(os.getenv('TIMEOUT', 10))
